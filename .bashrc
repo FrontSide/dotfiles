0b956de5d3ff7e4445bb1389486c78c2a0e67389
David <david@isan.engineer>
2018-02-08 20:12:52 +0000

    Add: tmux split-window alias

---
+++ .bashrc
@@ -0,0 +1,1 @@
+source tmux-extras.sh
---
+++ tmux-extras.sh
@@ -0,0 +1,4 @@
+# Run a command in a new split-off window inside tmux
+# by prepending the command with a comma (,)
+# e.g. , ping 8.8.8.8
+alias= ,='tmux split-window'
