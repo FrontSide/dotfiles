e4ed2961cbb9549306d15095a28fe1ee2f467e1d
David Rieger <david@isan.engineer>
2024-07-07 10:11:45 +0100

    feat: add nvim config

---
+++ nvim/README.md
@@ -0,0 +1,20 @@
+.config/nvim
+==============
+
+Tested with: 
+- neovim 0.9.5
+- go 1.22
+- node 1.12
+- npm 8.5.1
+- Ubuntu 22.04 LTS
+
+Uses: 
+- lazy (nvim plugin manager)
+- mason (nvim package manager for language tools)
+
+Other OS Requirments:
+- gcc, make, cmake
+- ripgrep
+
+Based on:
+https://www.josean.com/posts/how-to-setup-neovim-2024
---
+++ nvim/lua/personal/plugins/go.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+    end
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
+}
